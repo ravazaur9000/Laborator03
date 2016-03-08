59cf9791c71a39283e04f54cfe70afc2cece8eb0
Student User <student@cti.pub.ro>
2016-03-08 11:47:59 +0200

    implemented tasks for laboratory 03

---
+++ PhoneDialer/gen/ro/pub/cs/systems/pdsd/lab03/phonedialer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ro.pub.cs.systems.pdsd.lab03.phonedialer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
